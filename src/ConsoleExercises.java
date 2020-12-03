8d9bb68718ce67ebba0a16349444ee98691d0327
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-12-03 11:13:25 -0600

    starting Control Statements Lecture

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 
-        double pi = 3.14159;
-        System.out.printf("The value of pi us approximately %.2f%n", pi);
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi us approximately %.2f%n", pi);
 //
 //
 //
