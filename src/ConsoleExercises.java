079251f5c13a64219b4973f575b25c1414cd91b3
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-12-03 10:52:12 -0600

    finished going over console exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 
-//        double pi = 3.14159;
-//        System.out.printf("The value of pi us approximately %6.2f%n", pi);
+        double pi = 3.14159;
+        System.out.printf("The value of pi us approximately %.2f%n", pi);
 //
 //
 //
@@ -18,9 +18,9 @@ public class ConsoleExercises {
 //        String firstName = userInput.next();
 //        String middleName = userInput.next();
 //        String lastName = userInput.next();
-//        System.out.printf("first: %s, middle: %s, and last name: %s.%n", firstName, middleName, lastName);
+//        System.out.printf("first: %s,%n middle: %s,%n and last name: %s.%n", firstName, middleName, lastName);
 //
-//        System.out.println("Please enter a sentence of your choosing/");
+//        System.out.println("Please enter a sentence of your choosing.");
 //
 //        String userSentence = userInput.next();
 //        System.out.printf("Your sentence is: %s.%n",userSentence);
@@ -28,21 +28,21 @@ public class ConsoleExercises {
 //        String userSentenceNextLine = userInput.nextLine();
 //        System.out.printf("Your sentence is: %s.%n",userSentenceNextLine);
 
-          System.out.printf("enter the length of your classroom in feet%n");
-        float L = userInput.nextInt();
+        System.out.printf("enter the length of your classroom in feet%n");
+            float L = userInput.nextFloat();
         System.out.printf("enter the width of your classroom in feet%n");
-        float W = userInput.nextInt();
+            float W = userInput.nextFloat();
         System.out.printf("enter the Height of your classroom in feet%n");
-        float H = userInput.nextInt();
-        float A = L * W;
-        float P = (L * 2) + (W * 2);
-        float V = L * W * H;
-        System.out.printf("The classroom length is: %.2f feet%n" +
-                          "and a width of: %.2f feet%n" +
+            float H = userInput.nextFloat();
+            float A = L * W;
+            float P = (L * 2) + (W * 2);
+            float V = L * W * H;
+        System.out.printf("The classroom Length is: %.2f feet%n" +
+                          "and a Width of: %.2f feet%n" +
                           "a Height of: %.2f feet%n" +
-                          "The perimeter is %.2f Feet%n" +
+                          "The Perimeter is %.2f Feet%n" +
                           "The Area of the classroom is %.2f Square Feet.%n" +
-                          "The volume of your classroom is: %.2f Cubic Feet.#n", L, W, H, P, A, V);
+                          "The Volume of your classroom is: %.2f Cubic Feet.#n", L, W, H, P, A, V);
     }
 
 
