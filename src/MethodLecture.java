14d214e54394bdbc0b2b5b86a34f736b75d35225
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-12-04 17:01:35 -0600

    finished out day

--- src/MethodLecture.java
+++ src/MethodLecture.java
@@ -40,7 +40,7 @@ public class MethodLecture {
 //        countTo100Loop(1);
 //        countTo100(1);
 
-
+        System.out.println(fibonacci(0));
     }
 
     // ----------- DEFINING AND CALLING METHODS WITH VARIOUS INPUTS (examples)
@@ -94,29 +94,29 @@ public class MethodLecture {
 
     // ----------- RECURSION
 
-    //    public static void countTo100Loop(int num) {
-    //        for (int i = num; i <= 100; i += 1) {
-    //            System.out.println(i);
-    //        }
-    //    }
+//        public static void countTo100Loop(int num) {
+//            for (int i = num; i <= 100; i += 1) {
+//                System.out.println(i);
+//            }
+//        }
     //
-    //    public static int countTo100(int num) {
-    //        System.out.println(num);
-    //        return num == 100 ? num : countTo100(++num);
-    //    }
+//        public static int countTo100(int num) {
+//            System.out.println(num);
+//            return num == 100 ? num : countTo100(++num);
+//        }
     //
-    //    public static int powerOf10(int n) {
-    //        if (n == 0) {
-    //            return 1;
-    //        }
-    //        return powerOf10(n-1) * 10;
-    //    }
+//        public static int powerOf10(int n) {
+//            if (n == 0) {
+//                return 1;
+//            }
+//            return powerOf10(n-1) * 10;
+//        }
     //
-    //    public static int fibonacci(int n) {
-    //        if (n <= 1) {
-    //            return n;
-    //        }
-    //        return fibonacci(n-1) + fibonacci(n-2);
-    //    }
+        public static int fibonacci(int n) {
+            if (n <= 1) {
+                return n;
+            }
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
 
 }
