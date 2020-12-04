6329307695eec0de4e5577ba888cca7b26059b91
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-12-04 11:30:29 -0600

    finished strings exercise attempting bonus

--- src/Bob.java
+++ src/Bob.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Bob {
+   public static void main(String[] args) {
+
+       String bobsSentence = "";
+       Scanner userInput = new Scanner(System.in);
+       do {
+           System.out.println("Say something to our good friend Bob!");
+            bobsSentence = userInput.nextLine();
+           if (bobsSentence.trim().indexOf("?") > 0){
+               System.out.println("sure");
+           }else if (bobsSentence.trim().indexOf("!") > 0){
+               System.out.println("Whoa, Chill out!");
+           }else if (bobsSentence.trim().equalsIgnoreCase("")){
+               System.out.println("Fine. Be that way!");
+           }else if (bobsSentence.trim().equalsIgnoreCase("bye")){
+               System.out.println("...Bob didn't want to talk to you anyway!");
+               break;
+           }else {
+               System.out.println("Whatever.");
+           }
+       }while (!bobsSentence.equalsIgnoreCase("bye"));
+
+    }
+
 }
