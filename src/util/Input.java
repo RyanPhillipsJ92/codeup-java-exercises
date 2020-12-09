f970a29ddada56aaeb5a07860e33cd2b987f26f0
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-12-09 11:15:28 -0600

    saving exercise progress

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,32 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+    private Scanner sc = new Scanner(System.in);
+
+    String getString(){
+        System.out.println("Please enter a string.");
+        return  sc.nextLine();
+    }
+
+    boolean yesNo(){
+        System.out.println("Please enter 'y' or 'yes'.");
+         String userInput = sc.nextLine().trim();
+        return userInput.equalsIgnoreCase("y") ||
+                (userInput.equalsIgnoreCase("yes"));
+    }
+
+    int getInt(int min, int max){
+        int userNumber;
+        do {
+        System.out.printf("please enter a number between %d and %d%n",min, max);
+        userNumber = sc.nextInt();
+    }while(userNumber >= min && userNumber <= max);
+        return userNumber;
+    }
+
 }
+
+
+
