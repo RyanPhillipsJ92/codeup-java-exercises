e6e0c65a4348f9b0869e026733828a8866086276
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-12-14 13:34:38 -0600

    started square portion of exercise one

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,23 @@
 package shapes;
 
 public class Rectangle {
+
+    protected long length;
+    protected long width;
+    protected long area;
+
+
+    public Rectangle (long length, long width){
+    this.length = length;
+    this.width = width;
+    }
+
+    public long  getArea(long length, long width){
+        return length * width;
+    }
+
+    public long  getPerimeter(long length, long width){
+        return length * 2 + width * 2;
+    }
+
 }
