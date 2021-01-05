bfabdb5f8c371be15a3d7f404e1d1867df664a52
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-01-05 16:01:07 -0600

    used for practice

--- src/DebugWarmup.java
+++ src/DebugWarmup.java
@@ -1,3 +1,7 @@
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class DebugWarmup {
 
@@ -18,13 +22,27 @@ public class DebugWarmup {
 
         // HINT: You can fix the unexpected behavior by altering or adding a single line
         public static void main (String[]args){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Welcome to 'PRINT THE VEGETABLE COUNT' app!\n");
-            System.out.print("How many vegetables do you have? ");
-            int count = sc.nextInt();
-            System.out.print("What kind of vegetable? (use a singular noun, e.g. 'carrot' vs. 'carrots'): ");
-            String veggie = sc.next();
-            System.out.printf("You have %d %ss!\n\n", count, veggie);
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Welcome to 'PRINT THE VEGETABLE COUNT' app!\n");
+//            System.out.print("How many vegetables do you have? ");
+//            int count = sc.nextInt();
+//            System.out.print("What kind of vegetable? (use a singular noun, e.g. 'carrot' vs. 'carrots'): ");
+//            String veggie = sc.next();
+//            System.out.printf("You have %d %ss!\n\n", count, veggie);
+
+            int[] numbers = new int[]{1,2,3,4};
+            int result = 0;
+            for (int number : numbers) {
+                result *= number;
+            }
+            System.out.println(result);
+        }
+        public List<Integer> someFunction(final List<Integer> numbers){
+            List<Integer> result = new ArrayList<Integer>();
+            for (int i = numbers.size()- 1; i >= 0; i--){
+                result.add(numbers.get(i));
+            }
+            return result;
         }
     }
 
