59dda4ca0a1ed4b096819816355958c8732d9d8a
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-01-06 09:30:29 -0600

    used as template to try out code

--- src/DebugWarmup.java
+++ src/DebugWarmup.java
@@ -1,5 +1,4 @@
 import org.w3c.dom.Node;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -20,8 +19,9 @@ public class DebugWarmup {
         Process finished with exit code 0
      */
 
+
         // HINT: You can fix the unexpected behavior by altering or adding a single line
-        public static void main (String[]args){
+//        public static void main (String[]args) {
 //            Scanner sc = new Scanner(System.in);
 //            System.out.println("Welcome to 'PRINT THE VEGETABLE COUNT' app!\n");
 //            System.out.print("How many vegetables do you have? ");
@@ -29,20 +29,38 @@ public class DebugWarmup {
 //            System.out.print("What kind of vegetable? (use a singular noun, e.g. 'carrot' vs. 'carrots'): ");
 //            String veggie = sc.next();
 //            System.out.printf("You have %d %ss!\n\n", count, veggie);
+//        }
 
-            int[] numbers = new int[]{1,2,3,4};
-            int result = 0;
-            for (int number : numbers) {
-                result *= number;
-            }
-            System.out.println(result);
-        }
-        public List<Integer> someFunction(final List<Integer> numbers){
-            List<Integer> result = new ArrayList<Integer>();
-            for (int i = numbers.size()- 1; i >= 0; i--){
-                result.add(numbers.get(i));
-            }
-            return result;
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            int[] numbers = new int[]{1,2,3,4};
+//            int result = 0;
+//            for (int number : numbers) {
+//                result *= number;
+//            }
+//            System.out.println(result);
+//        }
+//        public List<Integer> someFunction(final List<Integer> numbers){
+//            List<Integer> result = new ArrayList<Integer>();
+//            for (int i = numbers.size()- 1; i >= 0; i--){
+//                result.add(numbers.get(i));
+//            }
+//            return result;
+//        }
+
+
 
